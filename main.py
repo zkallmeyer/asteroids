d7d636e63d7159531c40fce4417c4f89fd7717e6
zkallmeyer <zkallme@gmail.com>
2025-02-28 15:42:47 -0500

    created initial files

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+if __name__ == "__main__":
+    main()
